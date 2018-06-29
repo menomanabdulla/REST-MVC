95b1c17a68a457cbae34768f835b27ca1ad63597
nomanabdulla <noman.fcibappy@gmail.com>
2018-06-29 23:19:38 +0600

    Test Middleware and url end point it's work fine

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const app  = express()
 const PORT = process.env.PORT||3000
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
@@ -14,9 +16,26 @@ mongoose.connect('mongodb://menomanabdulla:noman123321BAPPY@ds121861.mlab.com:21
 const moviesRouter = require('./api/route/moviesRoute')
 
 //middlewire
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 app.use('/api/movies',moviesRouter)
 
 
+app.post('/',(req,res)=>{
+    const person = {
+        "name": req.body.name,
+        "email": req.body.email
+    }
+
+    res.status(201).json({
+        msg: 'Data Recived Successfully',
+        data: {
+            person
+        }
+    })
+
+})
 app.listen(PORT,()=>{
     console.log(`Listening on ${PORT}`)
 })
